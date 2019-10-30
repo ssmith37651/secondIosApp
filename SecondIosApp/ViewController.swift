59922c5988eb9ccac948b4161a14f3f3aba75f8d
Stephen Smith <ssmith37651@gmail.com>
2019-10-30 10:49:50 -0400

    Initial Commit

---
+++ SecondIosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondIosApp
+//
+//  Created by Stephen Smith on 10/30/19.
+//  Copyright © 2019 Stephen Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
