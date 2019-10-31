e56fde2f313db4249df589f51afb2e0c4d737845
Stephen Smith <ssmith37651@gmail.com>
2019-10-31 01:07:41 -0400

    added tip calculator logic

--- SecondIosApp/ViewController.swift
+++ SecondIosApp/ViewController.swift
@@ -18,6 +18,16 @@ class ViewController: UIViewController {
     // names of the different monsters
     var monsters = ["Astro", "Fluffy", "Munchie", "Squido"]
     
+    // IBOutlets are created as optionals
+    // This means that they may contain a value or
+    // They may be nil (null)
+    @IBOutlet weak var amountTextField: UITextField!
+    
+    @IBOutlet weak var tipPercentTextField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    
     // the equivalent of the onCreate method in Java Android Classes
     // called when the scene that it controls is accessed
     // initialization work is done in this method
@@ -50,6 +60,43 @@ class ViewController: UIViewController {
         // the new UIImage is the element in the monsters
         // array that matches the Index
         monsterImageView.image = UIImage(named: monsters[index])
+    }
+    
+    // gives the ViewController the ability to respond to
+    // the button being pushed
+    @IBAction func calculateTip(_ sender: UIButton) {
+        
+        var dTip = 0.0
+        var dAmount = 0.0
+        var dPercent = 0.0
+        
+        // get the values in the TextFields, if a value
+        // exists and the assign values to constants
+        // the TextFields are Optionals, so they could
+        // contain a value and they could also contain nil
+        if let amount = amountTextField.text, let percent = tipPercentTextField.text {
+            
+            // trim whitespace that may exist at the beginning
+            // or the ending of the constants
+            let trimmedAmount = amount.trimmingCharacters(in: .whitespaces)
+            let trimmedPercent = percent.trimmingCharacters(in: .whitespaces)
+            
+            // check to make sure the trimmed constants
+            // aren't empty
+            if (!trimmedAmount.isEmpty && !trimmedPercent.isEmpty){
+                
+                // Convert trimmed constants to Doubles
+                dAmount = Double(trimmedAmount)!
+                dPercent = Double(trimmedPercent)!
+                
+                // Calculate the tip
+                dTip = dAmount * dPercent
+            }
+        }
+        
+        // Convert calculated tip into a String
+        // displayed it in the label
+        tipLabel.text = "Tip is $\(String(dTip))"
     }
     
 }
